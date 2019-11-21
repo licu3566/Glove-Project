94f46a59542b41141032cc49eb4f8b174944243b
Lili Cushing-Quevli <licu3566@colorado.edu>
2019-11-21 11:21:32 -0700

    Initial upload
    moves back and forth along x-axis, have not tested flex sensor yet

---
+++ Glove_game/Glove_game.ino
@@ -0,0 +1,36 @@
+
+
+#include <Wire.h> // Must include Wire library for I2C
+#include <SparkFun_MMA8452Q.h>
+
+int flexSensor = 3;
+
+
+MMA8452Q accel; // Default MMA8452Q object create. (Address = 0x1D)
+//MMA8452Q accel(0x1C); // Initialize the MMA8452Q with an I2C address of 0x1C (SA0=0)
+//MMA8452Q accel(0x1D); // Initialize the MMA8452Q with an I2C address of 0x1D (SA0=1)
+
+//Source:
+//https://learn.sparkfun.com/tutorials/mma8452q-accelerometer-breakout-hookup-guide/all
+void setup() {
+  accel.init(); // Default init: +/-2g and 800Hz ODR
+  pinMode(flexSensor, INPUT);
+  Serial.begin(9600);
+
+
+
+
+}
+
+void loop() {
+  accel.read(); // Update acceleromter data
+  float  xAcceleration = accel.x; // Read in raw x-axis acceleration data
+  //  Serial.print("Acceleration on the x-axis is ");
+  //send information
+  Serial.print(analogRead(flexSensor));
+  Serial.print(",");
+  Serial.println(xAcceleration);
+
+
+
+}
